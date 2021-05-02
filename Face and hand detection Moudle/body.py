9cb6a7e61144d5761f88068cfcdb588d1d719c64
Tausif Mahmud <tmahmud44@gmail.com>
2021-05-03 01:58:58 +0600

    face and hand landmark code added

---
+++ Face and hand detection Moudle/body.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import mediapipe as mp
+import FaceAndHandDetectionModule as bfd
+
+cap = cv.VideoCapture(0)
+detector = bfd.
+
+while True:
+    isTrue , img = cap.read()
+
+    cv.imshow("body", img)
+
+    if cv.waitKey(1) & 0xFF == ord('f'):f
+        break 
